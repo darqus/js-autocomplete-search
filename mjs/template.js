0a8a24f507c6e52c3cb437ecb537ac85a05cf9cc
darqus <voladores@inbox.ru>
2022-09-10 19:03:06 +0300

    add \ in templates

--- mjs/template.js
+++ mjs/template.js
@@ -1,5 +1,6 @@
 // Static templates
-const initial = /*html*/ `<div class="container mt-5">
+const initial = /*html*/ `\
+<div class="container mt-5">
   <div class="row">
     <div class="col-lg-6 col-md-8 col-sm-10 m-auto">
       <h4 id="headerText" class="text-center mb-3"></h4>
@@ -16,7 +17,8 @@ const initial = /*html*/ `<div class="container mt-5">
   </div>
 </div>`
 
-const spinner = /*html*/ `<div class="d-flex">
+const spinner = /*html*/ `\
+<div class="d-flex">
   <div class="lds-css mt-5">
     <div class="lds-double-ring">
       <div></div>
@@ -25,20 +27,23 @@ const spinner = /*html*/ `<div class="d-flex">
   </div>
 </div>`
 
-const doneMsg = /*html*/ `<div class="alert alert-success my-3">
+const doneMsg = /*html*/ `\
+<div class="alert alert-success my-3">
   <h5>Done</h5>
   <hr/>
   <p>Data loaded</p>
 </div>`
 
-const warningMsg = /*html*/ `<div class="alert alert-warning my-3">
+const warningMsg = /*html*/ `\
+<div class="alert alert-warning my-3">
   <h5>Sorry</h5>
   <hr/>
   <p>Could not found data</p>
   <p>Try type another query</p>
 </div>`
 
-const noDataMsg = /*html*/ `<div class="alert alert-danger my-3">
+const noDataMsg = /*html*/ `\
+<div class="alert alert-danger my-3">
   <h5>Error</h5>
   <hr/>
   <p>Data not loaded</p>
@@ -46,7 +51,8 @@ const noDataMsg = /*html*/ `<div class="alert alert-danger my-3">
 
 // Dinamic templates
 const errorMsg = (url) => {
-  return /*html*/ `<div class="alert alert-danger my-3">
+  return /*html*/ `\
+<div class="alert alert-danger my-3">
   <h5>Sorry</h5>
   <hr/>
   <p>Could not fetch data from</p>
@@ -55,8 +61,8 @@ const errorMsg = (url) => {
 }
 
 const renderMain = (name, email, website, companyName, bs) => {
-  return /*html*/ `
-  <div class="card card-body my-3">
+  return /*html*/ `\
+<div class="card card-body my-3">
   <h5 class="card-subtitle my-2 text-warning">${name}</h5>
   <hr/>
   <div>Company: ${companyName}</div>
